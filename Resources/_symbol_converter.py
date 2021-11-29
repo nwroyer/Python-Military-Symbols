3ad7cbf19d14eb29bcaa7ecd8df31c58be713c42
N. W. Royer <nick.w.royer@gmail.com>
2021-11-28 17:03:51 -0800

    Updated recon and armor synonyms

--- Resources/_symbol_converter.py
+++ Resources/_symbol_converter.py
@@ -3,12 +3,15 @@ Utility script for converting SVGs in Inkscape-ready format to raw SVGs suitable
 """
 
 import json
-import os
+import os, sys
 import subprocess
 from pathlib import Path
 
-import src.military_symbol._json_filesystem
+if __name__ == '__main__':
+    sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), '../src')))
+    print(sys.path)
 
+from military_symbol import _json_filesystem
 
 def pack_svgs_into_json(current_working_directory: str, subdir: str = 'symbols', existing_input_file='',
                         output_file=''):
@@ -24,7 +27,7 @@ def pack_svgs_into_json(current_working_directory: str, subdir: str = 'symbols',
     print(f'Symbol dir: {symbol_dir}')
 
     total_file_count: int = 0
-    json_contents = src.military_symbol._json_filesystem.JSONFilesystem()
+    json_contents = _json_filesystem.JSONFilesystem()
 
     for subdir, dirs, files in os.walk(symbol_dir):
         for filename in files:
@@ -53,7 +56,7 @@ def pack_svgs_into_json(current_working_directory: str, subdir: str = 'symbols',
 
     print(f'Packed {total_file_count} SVGs into JSON')
 
-    new_json_file = src.military_symbol._json_filesystem.JSONFilesystem()
+    new_json_file = _json_filesystem.JSONFilesystem()
     new_json_file.read_from_file(os.path.join(current_working_directory, output_file))
 
 
@@ -128,7 +131,7 @@ def main():
 
     working_dir = os.path.dirname(os.path.realpath(__file__))
     print(working_dir)
-    convert_inkscape_to_svg(os.getcwd(), '')
+    # convert_inkscape_to_svg(os.getcwd(), '')
     pack_svgs_into_json(working_dir, 'symbols', output_file=os.path.join(working_dir,
                                                                          '../src/military_symbol/symbols.json'),
                         existing_input_file=os.path.join(working_dir, 'Symbol schema.json'))
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='military_symbol',
-    version='1.0.0',
+    version='1.0.1',
     description='Lightweight library for producing SVGs of NATO standard military symbols from NATO sidcs or natural-language descriptions',
     long_description=long_description,
     long_description_content_type='text/markdown',
